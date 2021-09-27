1d375b2cb245a6d57fd25e1e59f4c97bcf0df798
Rohith Chintala <35852989+RohithChintala@users.noreply.github.com>
2021-09-27 20:17:08 +0000

    change location of mycallback1

--- main.py
+++ main.py
@@ -8,6 +8,24 @@ GPIO.setup(27, GPIO.OUT)
 GPIO.setup(20, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
+def myCallback1(channel):
+  if channel == 20:
+	  pwm1.start(0)
+	  for dc in range(101):     
+		  pwm1.ChangeDutyCycle(dc)   # set duty cycle
+		  sleep(0.005)               # sleep 10 ms
+	  for dc in range(101):     
+		  pwm.ChangeDutyCycle(101-dc)   # set duty cycle
+		  sleep(0.005)               # sleep 10 ms
+  elif channel == 21:
+	  pwm2.start(0)
+	  for dc in range(101):     
+		  pwm2.ChangeDutyCycle(dc)   # set duty cycle
+		  sleep(0.005)               # sleep 10 ms
+	  for dc in range(101):     
+		  pwm2.ChangeDutyCycle(101-dc)   # set duty cycle
+		  sleep(0.005)               # sleep 10 ms
+
 pwm1= GPIO.PWM(4, 1) 
 pwm2= GPIO.PWM(17, 1) 
 pwm3= GPIO.PWM(27, 1) 
@@ -27,20 +45,4 @@ pwm2.stop()
 pwm3.stop()
 GPIO.cleanup()
 
-def myCallback1(channel):
-  if channel == 20:
-	  pwm1.start(0)
-	  for dc in range(101):     
-		  pwm1.ChangeDutyCycle(dc)   # set duty cycle
-		  sleep(0.005)               # sleep 10 ms
-	  for dc in range(101):     
-		  pwm.ChangeDutyCycle(101-dc)   # set duty cycle
-		  sleep(0.005)               # sleep 10 ms
-  elif channel == 21:
-	  pwm2.start(0)
-	  for dc in range(101):     
-		  pwm2.ChangeDutyCycle(dc)   # set duty cycle
-		  sleep(0.005)               # sleep 10 ms
-	  for dc in range(101):     
-		  pwm2.ChangeDutyCycle(101-dc)   # set duty cycle
-		  sleep(0.005)               # sleep 10 ms
+
