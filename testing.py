4eb8186a77c383ae0428e700a05e344a1469bd48
Rohith Chintala <35852989+RohithChintala@users.noreply.github.com>
2021-09-28 21:29:31 +0000

    s

--- testing.py
+++ testing.py
@@ -5,7 +5,7 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(4, GPIO.OUT)
 GPIO.setup(17, GPIO.OUT)
 GPIO.setup(27, GPIO.OUT)
-GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 pwm1= GPIO.PWM(4, 1)
 pwm2= GPIO.PWM(17, 1)
@@ -15,7 +15,8 @@ pwm1.start(0)
 
 def myCallback(channel):
   print(channel)
-  if channel == 16:
+  if channel == 26:
+    print('26 working')
     for d1 in range(100):
       pwm1.ChangeDutyCycle(d1)
       sleep(0.005)
@@ -24,7 +25,6 @@ def myCallback(channel):
       pwm1.ChangeDutyCycle(i)
       sleep(.005)
     pwm1.stop()
-
   if channel == 21:
     print('21 working')
     for d2 in range(101):
@@ -37,7 +37,7 @@ def myCallback(channel):
     pwm2.stop()
     
 
-GPIO.add_event_detect(16, GPIO.RISING, callback=myCallback, bouncetime=500)
+GPIO.add_event_detect(26, GPIO.RISING, callback=myCallback, bouncetime=500)
 GPIO.add_event_detect(21, GPIO.RISING, callback=myCallback, bouncetime=500)
 try:
  while 1:
